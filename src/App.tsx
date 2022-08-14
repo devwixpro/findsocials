75a8d924066dd489f7d719beb0f0c82fa71528c6
Vishva Gayan <vishvagayan@outlook.com>
2022-08-14 12:01:23 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import ReactPlayer from 'react-player'
 import './App.css';
-
+const videos = {
+  fb:
+    '<iframe src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2FUniversalTruth4U%2Fvideos%2F738458040745645%2F&show_text=false&width=476&t=0" width="200" height="200" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>'
+};
+function Iframe(props: any) {
+  return (
+    <div
+      dangerouslySetInnerHTML={{ __html: props.iframe ? props.iframe : "" }}
+    />
+  );
+}
 const player = () => {
   return (
     <div className='col'>
-      <ReactPlayer className="item" width={200} height={123} playing url='https://www.facebook.com/facebook/videos/10153231379946729/' />
-      <ReactPlayer className="item" width={200} height={123} playing url='https://www.facebook.com/facebook/videos/10153231379946729/' />
-      <ReactPlayer className="item" width={200} height={123} playing url='https://www.facebook.com/facebook/videos/10153231379946729/' />
-      <ReactPlayer className="item" width={200} height={123} playing url='https://www.facebook.com/facebook/videos/10153231379946729/' />
+      <Iframe iframe={videos["fb"]} allow="autoplay" />
+      <Iframe iframe={videos["fb"]} allow="autoplay" />
+      <Iframe iframe={videos["fb"]} allow="autoplay" />
+      <Iframe iframe={videos["fb"]} allow="autoplay" />
+      <Iframe iframe={videos["fb"]} allow="autoplay" />
     </div>
   );
 };
@@ -17,7 +27,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className='flex-grid'>
-          {player()}
           {player()}
           {player()}
           {player()}
