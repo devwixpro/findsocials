58644ebfa6497926e6c279228c7a450885a98fde
Vishva Gayan <vishvagayan@outlook.com>
2022-08-14 16:11:29 +0530

    intial

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 const videos = {
   fb:
-    '<iframe src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2FUniversalTruth4U%2Fvideos%2F738458040745645%2F&show_text=false&width=476&t=0" width="200" height="200" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>'
+    '<iframe src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2F104300845208279%2Fvideos%2F1518094558628116%2F&show_text=false&width=267&t=0" width="200" height="200" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>'
 };
 function Iframe(props: any) {
   return (
@@ -18,7 +18,6 @@ const player = () => {
       <Iframe iframe={videos["fb"]} allow="autoplay" />
       <Iframe iframe={videos["fb"]} allow="autoplay" />
       <Iframe iframe={videos["fb"]} allow="autoplay" />
-      <Iframe iframe={videos["fb"]} allow="autoplay" />
     </div>
   );
 };
@@ -27,6 +26,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className='flex-grid'>
+          {player()}
           {player()}
           {player()}
           {player()}
