c54fe82d6bb79cb8de11617d0d00a0159a715489
Vishva Gayan <vishvagayan@outlook.com>
2022-08-22 22:41:44 +0530

    Investigate and Find RC for Initial loading related Duplica e Api Calls -7

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 const videos = {
   fb:
-    '<iframe src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2F104300845208279%2Fvideos%2F1518094558628116%2F&show_text=false&width=267&t=0" width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>'
+    '<iframe src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2F104300845208279%2Fvideos%2F1518094558628116%2F&show_text=false&width=267&t=0" width="100" height="100" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>'
 };
 function Iframe(props: any) {
   return (
@@ -26,6 +26,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className='flex-grid'>
+          {player()}
           {player()}
           {player()}
           {player()}
