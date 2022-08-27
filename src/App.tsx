c398db82d72eb2ff3e1b16d458e826eb4bbb353e
Vishva Gayan <vishvagayan@outlook.com>
2022-08-27 09:38:50 +0530

    TX-33892 - As a Owner, i can see the agency specific fees in the marketing and leasing invoice - 1

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 const videos = {
   fb:
-    '<iframe src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2F104300845208279%2Fvideos%2F1518094558628116%2F&show_text=false&width=267&t=0" width="100" height="100" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>'
+    '<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2F100069083892544%2Fvideos%2F1427241784455077%2F&show_text=0&width=224" width="224" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>'
 };
 function Iframe(props: any) {
   return (
@@ -26,9 +26,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className='flex-grid'>
-          {player()}
-          {player()}
-          {player()}
           {player()}
           {player()}
           {player()}
